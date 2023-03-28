691c515f700ed234e336f0909f76c0383e4823b5
Hamudi Naanaa <hamudi.naanaa@tum.de>
2023-03-28 22:54:54 +0300

    Setup an inference server to run could inference on the duckiebot

--- local/yolo_object_detection.py
+++ local/yolo_object_detection.py
@@ -1,28 +1,44 @@
 # Networking
 from flask import Flask, request
-import io
 # ML
+import io
 import torch
+import torchvision.transforms as transforms
 from PIL import Image
-
+import pickle
+import time
 
 app = Flask(__name__)
 
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt', device='mps' if torch.has_mps else 'cpu')
+device = torch.device('mps' if torch.has_mps else 'cpu')
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='model/model.pt', device=device)
+gpu_transform = transforms.ToTensor()
+
+# Write a handler for a get request to the root of the server
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 @app.route('/detect_objects', methods=['POST'])
 def detect_objects():
     # Extract the incoming image
     try:
-        # Read the image
-        img = request.files['image'].read()
+        # Get the image from the request and unpickle it
+        img = pickle.loads(request.data)
+        # Convert img to ndarray
         img = Image.open(io.BytesIO(img))
+        # Convert the image to a pytorch tensor
+        img = gpu_transform(img).unsqueeze(0).to(device)
         
-        # Run the model
+        starttime = time.time()
+        # Run the model on the image
         pred = model(img)
+        endtime = time.time()
+        print(endtime - starttime)
         
-        # Show results
         pred.print() # profiling
+        return 'ok'
+        # Show results
         pred.show() # display
         print(pred.xywh) # print
         
@@ -32,4 +48,4 @@ def detect_objects():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=8000, debug=True, host='0.0.0.0')
--- packages/object_detection/src/object_detection_node.py
+++ packages/object_detection/src/object_detection_node.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 from __future__ import annotations
 
-import socket
 import rospy
 import pickle
+from time import perf_counter
+import requests
 
 # import torch
 # from PIL import Image 
@@ -11,13 +12,14 @@ import pickle
 from duckietown.dtros import DTROS, NodeType, TopicType, DTParam, ParamType
 from sensor_msgs.msg import CompressedImage
 
-COUNTER_FREQUENCY = 5
 
-IP = '420.13.37.69'
-PORT = 1337
+COUNTER_FREQUENCY = 10
 
-clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientSocket.connect((IP, PORT))
+IP = '192.168.0.176'
+PORT = 8000
+
+# clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# clientSocket.connect((IP, PORT))
 
 class ObjectDetectionNode(DTROS):
     def __init__(self) -> None:
@@ -33,9 +35,9 @@ class ObjectDetectionNode(DTROS):
             "~image", CompressedImage, self.cb_image, queue_size=1, buff_size="20MB"
         )
 
-        self._detected_objs = rospy.Publisher(
-            "~objects_detected", str, queue_size=1, dt_topic_type=TopicType.DRIVER
-        )
+        # self._detected_objs = rospy.Publisher(
+        #     "~objects_detected", String, queue_size=1, dt_topic_type=TopicType.DRIVER
+        # )
 
     def cb_image(self, msg) -> None:
         # def sort_func(tensor: torch.Tensor) -> float:
@@ -47,22 +49,22 @@ class ObjectDetectionNode(DTROS):
         # print(dataFromServer.decode())
         
         self.counter += 1
+        self.log(f"Received {self.counter} images")
         if self.counter == COUNTER_FREQUENCY:
+            # Capture start time
+            t_start = perf_counter()
             send_package = pickle.dumps(msg.data)
-            clientSocket.send(send_package)
+            self.log(f"Sending package of size {len(send_package)}")
+            # Send the image to the server and receive the response
+            response = requests.post(f"http://{IP}:{PORT}/detect_objects", data=send_package)
             self.counter = 0
-            decoded_data = clientSocket.recv(4_000_000).decode()
+            # Capture end time
+            t_end = perf_counter()
             # Not sure what kind of objects will be sent (if it is an array of integers, it will be awful)
-            self.log(str(len(decoded_data)))
+            self.log(f"Received response: {response} after {t_end - t_start} seconds")
+            
             return
-            # self.counter 
-            requests.post()
-            img = Image.fromarray(msg.data, 'RGB')
-            response = list(self._model(img))
-            response.sort(reverse=True, key=sort_func)
-            # self._detected_objs.publish(response)
-            self.log("Received image")
-    
+
 
 if __name__ == "__main__":
     node = ObjectDetectionNode()
